d78ba2cdb508ec7f814ff6b8e7185d41821f9afb
mkmktso <SpencerMAQ@users.noreply.github.com>
2022-12-09 23:50:20 +0800

    basic backend API already working

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,18 @@
+import time
+
 from flask import Flask
 from flask_cors import cross_origin
+from utils import dictionary
 
 app = Flask(__name__)
+
+
+class App:
+    def __init__(self):
+        dict_file_paths = dictionary.get_dict_file_paths()
+        all_data = dictionary.get_all_dict_data(dict_file_paths)
+        dict_instance = dictionary.RandomWord(all_data)
+        self.dict_instance = dict_instance
 
 
 @app.route("/api")
@@ -9,5 +20,12 @@ def index():
     return "Api Root"
 
 
+@app.route("/api/random_word", methods=["GET"])
+def get_random_word():
+    return dict_instance.get_rand_word_and_freq()
+    # return {"a": 1, "id": id(rand)}
+
+
 if __name__ == "__main__":
+    dict_instance = App().dict_instance
     app.run(debug=True)
--- backend/utils/dictionary.py
+++ backend/utils/dictionary.py
@@ -2,66 +2,125 @@ import glob
 import json
 import os
 import random
-from pprint import pprint
+from itertools import permutations
+
+import requests
 
 
 class RandomWord:
+    """
+    Get a random word with its associated information
+    from a dictionary file
+
+    Args:
+        data: Dictionary of English dict data
+    """
+
     def __init__(self, data: dict) -> None:
         self.data = data
+        self.keys = list(data.keys())
 
-        keys = list(data.keys())
-        word = random.choice(keys)
-        entry = data.get(word)
-        self.word = word
+    def get_random_word(self) -> str:
+        return random.choice(self.keys)
 
-    def get_random_word(self):
-        """
+    def get_rand_word_and_freq(self) -> dict:
+        """Get a random word and its corresponding frequency
+
         Returns:
-            tuple: A random word and its corresponding
-            dictionary entry
+            dict: {"word": food, "freq": 220.0.1}
         """
+        word = random.choice(self.keys)
+        freq = self.get_frequency(word)
+        return {"word": word, "freq": freq}
 
-        keys = list(self.data.keys())
-        word = random.choice(keys)
-        entry = self.data.get(word)
-        return (word, entry)
+    def get_definition(self, word: str) -> dict:
+        return self.data.get(word)
 
-    def get_pos(self):
-        defn_block: dict = self.data.get(self.word)
-        meanings: dict = defn_block.get("MEANINGS", None)
-        keys = sorted(list(meanings.keys()))
-        # print("keys: ", keys)
-        if keys:
-            print("meanings: ", meanings)
-            print("pos: ", meanings.get(keys[0][0][0], None))
-            return meanings.get(keys[0][0][0], None)
+    def get_part_of_speech(self, word: str) -> str or None:
+        defn: dict = self.get_definition(word)
+        meanings: dict = defn.get("MEANINGS", None)
+        if meanings:
+            meaning_keys = meanings.keys()
+            first_key = list(meaning_keys)[0]
+
+        if first_key:
+            return meanings.get(first_key)[0]
 
         return None
 
-    def get_frequency(self):
-        pass
+    def get_frequency(self, word) -> str:
+        data_muse = f"https://api.datamuse.com/words?sp={word}&qe=sp&md=fr&max=1"
+        res = requests.get(data_muse, timeout=5)
+        if res:
+            res = res.json()
+            res = res[0]
+            res = res.get("tags", None)
+            res: str = res[-1]
+            res = float(res.split(":")[-1])
+            return res
+        else:
+            return None
+
+    def get_subwords(self, word) -> list:
+        site = f"https://www.degraeve.com/subwords.php?w={word}"
+        resp = requests.get(site, timeout=5)
+        if resp:
+            resp = resp.json()
+
+    # @property
+    # def permutations(self):
+    #     return permutations(self.word)
 
 
-def main():
-    # load_random_dict()
-    pass
+def get_dict_file_paths(debug=False) -> list:
+    """Return the file paths of all the dictionary files
+
+    Returns:
+        list: List of file paths
+    """
+
+    if debug:
+        return glob.glob(os.path.join("..", "dictionary_files/D*.json"))
+    return glob.glob("dictionary_files/D*.json")
 
 
-def load_random_dict():
-    """Get the data (in dict format) of a random json file
+def get_random_dict_path(file_paths: str) -> str:
+    return random.choice(file_paths)
+
+
+def dict_path_to_dict_data(file_path: str) -> dict:
+    """
+    Get the data (in dict format) of a random json file
 
     Returns:
         dictionary:
     """
-    dict_file_paths = glob.glob(os.path.join("..", "dictionary_files/D*.json"))
-    rand_file = random.choice(dict_file_paths)
-    with open(rand_file, "r", encoding="utf8") as fh:
+    with open(file_path, "r", encoding="utf8") as fh:
         dict_data = json.load(fh)
 
     return dict_data
 
 
+def get_all_dict_data(file_paths: str) -> dict:
+    consolidated_dict = dict()
+    for path in file_paths:
+        with open(path, "r", encoding="utf8") as fh:
+            data: dict = json.load(fh)
+            consolidated_dict = consolidated_dict | data
+
+    return consolidated_dict
+
+
 if __name__ == "__main__":
-    rand_dict = load_random_dict()
-    d = RandomWord(rand_dict)
-    d.get_pos()
+    dict_file_paths = get_dict_file_paths(debug=True)
+    # rand_path = get_random_dict_path(dict_file_paths)
+    # rand_dict = dict_path_to_dict_data(rand_path)
+    # d = RandomWord(rand_dict)
+    # print(d.word)
+    # print(d.part_of_speech)
+    # print(d.frequency)
+    # print(len(list(d.permutations)))
+
+    all_data = get_all_dict_data(dict_file_paths)
+    rand = RandomWord(all_data)
+    print(rand.get_rand_word_and_freq())
