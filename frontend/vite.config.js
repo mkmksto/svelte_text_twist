1d4b08d2c48cab16637c1a2ab517a61e0d422da3
mkmktso <SpencerMAQ@users.noreply.github.com>
2022-12-18 20:21:30 +0800

    Revert "try new vite config"
    This reverts commit 94f7fd00b3da76f946c828dd197d49dac9d82b98.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,7 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
-    // https://stackoverflow.com/questions/66863200/changing-the-input-and-output-directory-in-vite
-    // https://stackblitz.com/edit/vitejs-output-file-paths?file=package.json,vite.config.js
-    build: {
-        outDir: '../../backend/templates',
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name.split('.').at(1)
-                    if (/png|jpe?g|svg|gif|tiff|bm|ico/i.test(extType)) {
-                        extType = 'img'
-                    }
-                    return `../../${extType}/[name]-[hash][extname]`
-                },
-                chunkFileNames: '../../static/js/[name]-[hash].js',
-                entryFileNames: '../../static/js/[name]-[hash].js',
-            },
-        },
-    },
+  plugins: [svelte()]
 })
