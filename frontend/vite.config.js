d719ffcba7fa941c4f50deffab100124f0e2c551
mkmktso <SpencerMAQ@users.noreply.github.com>
2022-12-19 00:33:14 +0800

    rough incomplete vite build to backend to implement: auto refactor the references inside index.html to flask syntax, i.e. url_for

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+    plugins: [svelte()],
+    // https://stackoverflow.com/questions/71180561/vite-change-ouput-directory-of-assets
+    build: {
+        // outDir: '../backend/templates',
+        // assetsDir: 'static',
+        // publicPath: '/static',
+        //     rollupOptions: {
+        //         output: {
+        //             assetFileNames: (assetInfo) => {
+        //                 let extType = assetInfo.name.split('.').at(1)
+        //                 if (/png|jpe?g/i.test(extType)) {
+        //                     extType = 'img'
+        //                 }
+        //                 return path.resolve(`../static/${extType}/[name]-[hash][extname]`)
+        //             },
+        //             chunkFileNames: '',
+        //         },
+        //     },
+    },
 })
